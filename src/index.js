4aac9fd7ddac8fa3d07b343823ce9c510866c829
Christabel Quaye <christabel.quaye@amalitech.com>
2023-06-16 17:23:41 +0000

    Revert "text"
    This reverts commit f0adbe0115841abd3bb9f093824266ea97fd82ed.

--- src/index.js
+++ src/index.js
@@ -1,3 +1,11 @@
+<<<<<<< Updated upstream
+import React from "react";
+import ReactDOM from "react-dom/client";
+import "./index.css";
+import App from "./App";
+import reportWebVitals from "./reportWebVitals";
+import { BrowserRouter } from "react-router-dom";
+=======
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
@@ -6,9 +14,17 @@ import reportWebVitals from './reportWebVitals';
 import { Provider } from 'react-redux';
 import { store } from './store/app/store';
 import { BrowserRouter } from 'react-router-dom';
+>>>>>>> Stashed changes
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
+<<<<<<< Updated upstream
+  <React.StrictMode>
+    <BrowserRouter>
+      <App />
+    </BrowserRouter>
+  </React.StrictMode>
+=======
   <BrowserRouter>
     <React.StrictMode>
       <Provider store={store}>
@@ -16,6 +32,7 @@ root.render(
       </Provider>
     </React.StrictMode>
   </BrowserRouter>
+>>>>>>> Stashed changes
 );
 
 // If you want to start measuring performance in your app, pass a function
