f0adbe0115841abd3bb9f093824266ea97fd82ed
Felix-Danso <117346298+Felix-Danso@users.noreply.github.com>
2023-06-16 16:49:56 +0000

    text

--- src/index.js
+++ src/index.js
@@ -1,11 +1,3 @@
-<<<<<<< Updated upstream
-import React from "react";
-import ReactDOM from "react-dom/client";
-import "./index.css";
-import App from "./App";
-import reportWebVitals from "./reportWebVitals";
-import { BrowserRouter } from "react-router-dom";
-=======
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
@@ -14,17 +6,9 @@ import reportWebVitals from './reportWebVitals';
 import { Provider } from 'react-redux';
 import { store } from './store/app/store';
 import { BrowserRouter } from 'react-router-dom';
->>>>>>> Stashed changes
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
-<<<<<<< Updated upstream
-  <React.StrictMode>
-    <BrowserRouter>
-      <App />
-    </BrowserRouter>
-  </React.StrictMode>
-=======
   <BrowserRouter>
     <React.StrictMode>
       <Provider store={store}>
@@ -32,7 +16,6 @@ root.render(
       </Provider>
     </React.StrictMode>
   </BrowserRouter>
->>>>>>> Stashed changes
 );
 
 // If you want to start measuring performance in your app, pass a function
